4e5e67191184cf3a27b9594b29584073ad5d4fee
theendisfar <r3steve@gmail.com>
2021-03-14 10:40:40 -0400

    project ready

--- btre/settings.py
+++ btre/settings.py
@@ -142,3 +142,11 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
     
 }
+
+# email config
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
+EMAIL_USE_TLS=True
